44aded6d65173a70cab4bfbb2476d1235bba46fd
Carter Hawkins <Carterhawkins93@gmail.com>
2022-09-20 19:22:12 -0400

    First Part Complete

---
+++ Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Carter Hawkins on 9/20/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
